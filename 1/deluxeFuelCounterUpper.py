3053eb53c07ee46babf8671828e5ed48aeb9b6f3
Tony Lofreso <anthonylofreso@westfieldgrp.com>
2019-12-02 13:41:58 -0500

    made a slight improvement. Python understands order of operations. removed un-needed parentheses. Also added new Deluxe fuel counter upper

--- 1/deluxeFuelCounterUpper.py
+++ 1/deluxeFuelCounterUpper.py
@@ -8,7 +8,7 @@ for module in modules:
     module_mass.append(int(module))
 
 for module in module_mass:
-    required_fuel.append(int(module / 3 - 2))
+    required_fuel.append(int((module / 3)) - 2)
 
 final_fuel = sum(required_fuel)
 print(final_fuel)
